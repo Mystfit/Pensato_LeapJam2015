e27bf6116bd60b1ad04cbb79227b3ace675fa162
Byron Mallett <byronated@gmail.com>
2015-10-20 19:40:44 +1300

    Added folders for UI scripts and categories. Created auto-sorting radial layout. In progress of adding dynamic width placements to radial layout

---
+++ Assets/Scripts/UI/LiveSong.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Utils;
+using System.Collections;
+
+public class LiveSong : LiveProxy {
+
+    //ASSUME: Have track holder
+    private Transform trackParent;
+
+    void Awake () {
+        trackParent = gameObject.FindInChildren("tracks").transform;
+    }
+}
